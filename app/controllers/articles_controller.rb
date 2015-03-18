9784ebb46d5591bd713d4feae11b6af593c92c9e
Kristel Teng <kristelteng@gmail.com>
2015-03-18 15:26:02 -0400

    Questions displayed on the landing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,4 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all 
-  end
-
   def new 
 
   end 
@@ -11,13 +7,10 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     @article.save
-    redirect_to @article
+    redirect_to root_path
   end 
 
-  def show
 
-    @article = Article.find(params[:id])
-  end
 
 private
   def article_params
