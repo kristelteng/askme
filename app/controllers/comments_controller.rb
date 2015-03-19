d7166a441a37878218e49dba6e70db82a3a5026d
Kristel Teng <kristelteng@gmail.com>
2015-03-19 00:08:11 -0400

    delete feature for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,16 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end 
 
+  def destroy
+    @article = Article.find(params[:article_id])
+
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to root_path
+
+  end 
+
 private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
