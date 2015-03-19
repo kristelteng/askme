abac5d7f431d49488ad4fe681b8f8325fd78f598
Kristel Teng <kristelteng@gmail.com>
2015-03-19 13:36:53 -0400

    create routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -18,6 +12,7 @@ Rails.application.routes.draw do
   end 
   
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 
 
