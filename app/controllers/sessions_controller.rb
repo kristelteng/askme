dadcb3713666caafcea84c60af383c826fe94b0b
Kristel Teng <kristelteng@gmail.com>
2015-03-19 14:28:23 -0400

    sessions controller and view working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logged in!'
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'logged out!'
   end
 end
