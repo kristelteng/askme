d92421a0cc278dcc34eea7c2fa40e32d01d10054
Kristel Teng <kristelteng@gmail.com>
2015-03-19 21:29:12 -0400

    add user_id to answers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320012234) do
+ActiveRecord::Schema.define(version: 20150320012519) do
 
   create_table "answers", force: true do |t|
     t.string   "commenter"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150320012234) do
     t.integer  "question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id"
