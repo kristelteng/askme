89c15b53d536ae77de8ca682b024a5a5da917077
Kristel Teng <kristelteng@gmail.com>
2015-03-19 20:42:06 -0400

    add a user_id to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320003304) do
+ActiveRecord::Schema.define(version: 20150320004115) do
 
   create_table "answers", force: true do |t|
     t.string   "commenter"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150320003304) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
