a7bdf2ef89c15df531827576bcd2ceec3ee501ac
Kristel Teng <kristelteng@gmail.com>
2015-03-20 14:31:34 -0400

    remove ensure_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-
-  # def ensure_logged_in
-  #   unless current_user
-  #     flash[:alert] = "Please log in"
-  #     redirect_to new_session_path
-  #   end
-  # end
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
